5febe256bcbbb94c8036a357ac05e5c3ddbf9363
hf <fuhi1983@gmail.com>
2016-07-09 17:25:37 +0900

    add ChileViewController into ScrollMenuViewController

---
+++ ScrollMenuViewController/ScrollMenuViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ScrollMenuViewController.swift
+//  ScrollMenuViewController
+//
+//  Created by hf on 2016/07/09.
+//  Copyright © 2016年 swift-studying.com. All rights reserved.
+//
+
+import UIKit
+
+/**
+ ScrollMenuViewController
+ */
+class ScrollMenuViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor.blueColor()
+    }
+}
